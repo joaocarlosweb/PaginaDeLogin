5b8773e0b6339ee32a4f484a27d6e704df740ef7
joaocarlosweb <joaocarloswebcod@gmail.com>
2023-12-28 15:00:30 -0300

    atualizado

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 //
-
+let nome= document.querySelector(".nome").value;
 function logar(){
 
     let email= document.querySelector(".email").value;
     let senha= document.querySelector(".senha").value;
+    
     
     if(email== "admin@gmail" && senha == 1234){
         alert("você logou com sucesso")
