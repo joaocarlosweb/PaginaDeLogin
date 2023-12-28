f796e856d0de2aae0d6dd96ed907fd008faa960b
joaocarlosweb <joaocarloswebcod@gmail.com>
2023-12-28 13:37:50 -0300

    Página de Login

---
+++ index.js
@@ -0,0 +1,17 @@
+
+//
+
+function logar(){
+
+    let email= document.querySelector(".email").value;
+    let senha= document.querySelector(".senha").value;
+    
+    if(email== "admin@gmail" && senha == 1234){
+        alert("você logou com sucesso")
+        location.href="home.html"
+    }else{
+        alert("senha ou email incorreto")
+        
+    }
+
+}
