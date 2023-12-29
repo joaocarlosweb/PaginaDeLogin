6d8b7b44a8056e7eaa9663c956b4e315ac9ca128
joaocarlosweb <joaocarloswebcod@gmail.com>
2023-12-29 09:48:59 -0300

    atualizado

--- home.js
+++ home.js
@@ -2,6 +2,7 @@
 const button = document.querySelector("button")
 const modal= document.querySelector(".modal")
 const btn_closed= document.querySelector(".modal .btn_closed")
+const btn_seguir= document.querySelector(".modal .btn_seguir")
 
 button.onclick =function clicou(){
     modal.showModal()
@@ -10,5 +11,10 @@ button.onclick =function clicou(){
 
 btn_closed.onclick = function sair(){
     modal.close()
+    
+}
+btn_seguir.onclick =function seguir(){
+    
+    location.href="inicio.html"
 }
 
